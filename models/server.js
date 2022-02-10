5c0e237030ffeb25042abd1bdfc4a68624b6f29d
reliek21 <keilerguardo@gmail.com>
2022-02-10 17:16:00 -0500

    Connecting database

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 
 class Server {
@@ -8,11 +9,19 @@ class Server {
         this.port = process.env.PORT;
         this.userRoutes = '/api/users';
 
+        // database connection
+        this.connectDB();
+
+
         // middlewares
         this.middlewares();
 
         // routes
         this.routes();
+    }
+
+    async connectDB() {
+        await dbConnection();
     }
 
     middlewares() {

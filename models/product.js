4f5b480e62588ad182cb08fdfb5b25ab18da5d3d
reliek21 <keilerguardo@gmail.com>
2022-02-11 23:47:35 -0500

    Structuring product requirements

--- models/product.js
+++ models/product.js
@@ -2,36 +2,45 @@ const { Schema, model } = require('mongoose');
 
 
 const ProductSchema = Schema({
-    // code: {
-    //     type: String,
-    //     required: true,
-    // },
+    id: {
+        type: Number,
+        require: [true, 'ID is required']
+    },
+    code: {
+        type: String,
+        unique: true,
+        require: [true, 'Code is required'],
+        min: [true, 'The code must be at minimum 4 characters in length'],
+        max: [true, 'The code must have a maximum of 10 characters']
+    },
     name: {
         type: String,
-        required: [true, 'The name is required'],
+        unique: true,
+        required: [true, 'Name is required'],
     },
     description: {
         type: String,
+        required: [true, 'Description is required'],
     },
-    price: {
-        type: Number,
-        default: 0
-    },
-    active: {
-        type: Boolean,
-        default: true,
-        required: true
+    brand: {
+        type: String,
+        required: [true, 'Brand is required'],
     },
     category: {
         type: Schema.Types.ObjectId,
         ref: 'Category',
-        require: true
+        required: [true, 'Category is required'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        default: 0
     }
 });
 
 
 ProductSchema.methods.toJSON = function () {
-    const { __v, ...data } = this.toObject();
+    const { _id, __v, ...data } = this.toObject();
     return data;
 }
 
